a9792d3d26a31fdbbe835074ffe984f048511700
RomanBass <49683918+RomanBass@users.noreply.github.com>
2020-09-26 09:44:11 +0300

    поменял константы

--- js/stat.js
+++ js/stat.js
@@ -1,29 +1,29 @@
 /* eslint-disable no-var */
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGHT = 270;
-var CLOUD_X = 100;
-var CLOUD_Y = 10;
-var GAP = 10;
-var TITLE_GAP = 20;
-var FONT_GAP = 33;
-var NAME_HEIGHT = 15;
-var GISTOGRAM_HEIGHT = 85;
-var TIME_HEIGHT = 40;
-var BAR_HEIGHT_MAX = 150;
-var BAR_WIDTH = 40;
-var BARS_DISTANCE = 90;
-var GISTOGRAM_LEFT = 40;
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const GAP = 10;
+const TITLE_GAP = 20;
+const FONT_GAP = 33;
+const NAME_HEIGHT = 15;
+const GISTOGRAM_HEIGHT = 85;
+const TIME_HEIGHT = 40;
+const BAR_HEIGHT_MAX = 150;
+const BAR_WIDTH = 40;
+const BARS_DISTANCE = 90;
+const GISTOGRAM_LEFT = 40;
 
-var renderCloud = function (ctx, x, y, color) {
+const renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getMaxElement = function (arr) {
-  var maxElement = arr[0];
-  for (var i = 1; i < arr.length; i++) {
+const getMaxElement = function (arr) {
+  let maxElement = arr[0];
+  for (let i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
@@ -31,7 +31,7 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-var getRandomSaturation = function () {
+const getRandomSaturation = function () {
   return Math.floor(Math.random() * 101);
 };
 
@@ -45,9 +45,9 @@ window.renderStatistics = function (ctx, players, times) {
   ctx.fillText(`Ура вы победили!`, CLOUD_X + TITLE_GAP, CLOUD_Y + FONT_GAP);
   ctx.fillText(`Список результатов:`, CLOUD_X + TITLE_GAP, CLOUD_Y + FONT_GAP + TITLE_GAP);
 
-  var maxTime = getMaxElement(times);
+  let maxTime = getMaxElement(times);
 
-  for (var i = 0; i < players.length; i++) {
+  for (let i = 0; i < players.length; i++) {
 
     ctx.fillStyle = `#000`;
     ctx.fillText(players[i], CLOUD_X + GISTOGRAM_LEFT + BARS_DISTANCE * i, CLOUD_Y + CLOUD_HEIGHT - NAME_HEIGHT);
